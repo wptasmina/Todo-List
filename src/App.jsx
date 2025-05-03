255fc4606a539f29d64d8eb3016de27d02be283a
wptasmina <wptasmina@gmail.com>
2025-05-04 01:28:30 +0600

    add todo from

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import ToDoList from './components/ToDoList'
 
 export default function App() {
   return (
-    <>
+    <div className='flex justify-center items-center flex-col min-h-screen bg-amber-50'>
     <ToDoList/>
-    </>
+    </div>
   )
 }
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -2,12 +2,25 @@ import React from 'react'
 
 export default function ToDoList() {
   return (
-    <div className='flex justify-center items-center flex-col min-h-screen bg-amber-50'>
+    <div className='space-y-4'>
+        <h1 className='text-4xl font-extrabold text-gray-800 text-center'>ToDo List</h1>
+        <div className='space-x-4'>
+            <input 
+            type="text" 
+            placeholder='add todo'
+            className='placeholder:text-gray-700 border px-4 py-2 rounded-md' 
+            />
+
+            <button className='text-white text-xl font-medium bg-green-500 hover:green-700 px-4 py-2 rounded-md cursor-pointer'>Add</button>
+        </div>
         <div className=''>
-            <h1 className='text-txl font-extrabold text-gray-800'>ToDo List</h1>
-            <div className=''>
-                <div className=''></div>
-            </div>
+            <ul>
+                <li className='flex justify-between items-center px-4 py-2'>Item 1</li>
+                <li className='flex justify-between items-center px-4 py-2'>Item 2</li>
+                <li className='flex justify-between items-center px-4 py-2'>Item 3</li>
+            </ul>
+        </div>
+        <div className=''>
         </div>
     </div>
   )
